b5c20bdef01c0d650ac141a2fb9aa33809bbf016
kyle <dev.kyleulman@gmail.com>
2022-10-21 17:29:32 -0400

    add update fn, refactor pref data to body obj

--- src/lib/rsvp/Rsvp.svelte
+++ src/lib/rsvp/Rsvp.svelte
@@ -9,7 +9,7 @@
 		value: string;
 	}
 
-	let dietaryPreferences: Array<DietaryPreferencesInput> = [
+	let dietaryPreferencesInput: Array<DietaryPreferencesInput> = [
 		{
 			label: 'No Restrictions',
 			id: 'none',
@@ -37,77 +37,112 @@
 		}
 	];
 
-	let rsvpResponse: 'yes' | 'no' | 'maybe' | undefined = undefined;
-	let shadowPreferences: Array<string> = [];
-	let selectedPreferences: Array<string> = [];
-	let specialDietaryRequests: string = '';
+	interface Body {
+		rsvpResponse: 'yes' | 'no' | 'maybe' | undefined;
+		dietaryPreferences: Array<string>;
+		specialDietaryRequests: string;
+	}
+
+	let body: Body = {
+		rsvpResponse: undefined,
+		dietaryPreferences: [],
+		specialDietaryRequests: ''
+	};
+
+	let shadowBody: Body = body;
+
 	let returnMsg: string = '';
+	let nudgeTimeout: ReturnType<typeof setTimeout>;
 	let putTimeout: ReturnType<typeof setTimeout>;
-	let wasNudged: Boolean = false;
+
+	async function resetPrefs() {
+		body.dietaryPreferences = [];
+		body.specialDietaryRequests = '';
+	}
+
+	async function update() {
+		clearTimeout(putTimeout);
+
+		putTimeout = setTimeout(() => {
+			// console.log(body);
+		}, 1500);
+	}
 
 	function handleForm(e: Event) {
-		if (rsvpResponse) {
-			// PUT rsvpResponse
+		if (body.rsvpResponse === 'no') {
+			iconName = 'frown';
+			returnMsg = `Sorry you can’t make it. We’ve notified [hostPreferredName], and hopefully we’ll see you next time.`;
+			resetPrefs();
+		} else if (body.rsvpResponse === 'maybe') {
+			iconName = 'meh';
+			returnMsg = `We’ve notified [hostPreferredName]. Please make a decision soon, since we need a final head count as soon as possible.`;
+			resetPrefs();
+		} else if (body.rsvpResponse === 'yes') {
+			iconName = 'smile';
+			returnMsg = '';
 
-			if (rsvpResponse === 'no') {
-				iconName = 'frown';
-				returnMsg = `Sorry you can’t make it. We’ve notified [hostPreferredName], and hopefully we’ll see you next time.`;
+			// Nudge user to make a selection after 10 seconds of inactivtiy
+			// nudgeTimeout = setTimeout(() => {
+			// 	returnMsg =
+			// 		'We need to know your dietary preferences so that we can customize the menu to meet your needs.';
+			// }, 10 * 1000);
+
+			const clickedInput = e.target as HTMLInputElement;
+
+			// No restrictions is clicked
+			if (clickedInput.id === 'none') {
+				// checked
+				if (clickedInput.checked === true) {
+					body.dietaryPreferences = ['none'];
+				}
+				// unchecked
+				else {
+					// reload shadow selection
+					body = {
+						...body,
+						dietaryPreferences: shadowBody.dietaryPreferences
+					};
+				}
 			}
-
-			if (rsvpResponse === 'maybe') {
-				iconName = 'meh';
-				returnMsg = `We’ve notified [hostPreferredName]. Please make a decision soon, since we need a final head count as soon as possible.`;
-			}
-
-			if (rsvpResponse === 'yes') {
-				iconName = 'smile';
-				returnMsg = '';
-
-				// Nudge user to make a selection after 10 seconds of inactivtiy
-				if (wasNudged === false) {
-					setTimeout(() => {
-						returnMsg =
-							'We need to know your dietary preferences so that we can customize the menu to meet your needs.';
-					}, 10 * 1000);
-
-					wasNudged = true;
+			// Any other dietaryPreferencesInput are clicked & no restrictions has check
+			else if (
+				new RegExp('vegetarian|vegan|glutenFree|lowCarb', 'gm').test(
+					clickedInput.id
+				)
+			) {
+				if (body.dietaryPreferences.includes('none')) {
+					body.dietaryPreferences = body.dietaryPreferences.filter(
+						(pref) => pref !== 'none'
+					);
 				}
 
-				const clickedInput = e.target as HTMLInputElement;
-
-				// No restrictions is clicked
-				if (clickedInput.id === 'none') {
-					// checked
-					if (clickedInput.checked === true) {
-						selectedPreferences = ['none'];
-					}
-					// unchecked
-					else {
-						// reload shadow selection
-						selectedPreferences = shadowPreferences;
-					}
-				}
-				// Any other prefs are clicked & no restrictions has check
-				else if (
-					new RegExp(
-						'vegetarian|vegan|glutenFree|lowCarb',
-						'gm'
-					).test(clickedInput.id)
+				if (
+					clickedInput.id === 'vegetarian' &&
+					clickedInput.checked === true
 				) {
-					if (selectedPreferences.includes('none')) {
-						selectedPreferences = selectedPreferences.filter(
-							(pref) => pref !== 'none'
-						);
-					}
-
-					shadowPreferences = selectedPreferences;
+					body.dietaryPreferences = body.dietaryPreferences.filter(
+						(pref) => pref !== 'vegan'
+					);
 				}
+
+				if (
+					clickedInput.id === 'vegan' &&
+					clickedInput.checked === true
+				) {
+					body.dietaryPreferences = body.dietaryPreferences.filter(
+						(pref) => pref !== 'vegetarian'
+					);
+				}
+
+				shadowBody = {
+					...shadowBody,
+					dietaryPreferences: body.dietaryPreferences
+				};
 			}
 		}
+		console.log('Shadow bdy', shadowBody);
 
-		console.log('Is Going?:', rsvpResponse);
-		console.log('Shadow Preferences:', shadowPreferences);
-		console.log('Selected Preferences:', selectedPreferences);
+		update();
 	}
 </script>
 
@@ -127,35 +162,35 @@
 			id="yes"
 			name="rsvpResponse"
 			value="yes"
-			bind:group={rsvpResponse}
+			bind:group={body.rsvpResponse}
 		/>
 		<Toggle
 			label="No"
 			id="no"
 			name="rsvpResponse"
 			value="no"
-			bind:group={rsvpResponse}
+			bind:group={body.rsvpResponse}
 		/>
 		<Toggle
 			label="Maybe"
 			id="maybe"
 			name="rsvpResponse"
 			value="maybe"
-			bind:group={rsvpResponse}
+			bind:group={body.rsvpResponse}
 		/>
-		{#if rsvpResponse === 'yes'}
+		{#if body.rsvpResponse === 'yes'}
 			<hr />
 			<h2 class="text-xl">Dietary preferences</h2>
-			{#each dietaryPreferences as { label, id, value }}
+			{#each dietaryPreferencesInput as { label, id, value }}
 				<!-- TODO: Isolate to component -->
 				<div>
 					<label for={id}>{label}</label>
 					<input
 						type="checkbox"
 						{id}
-						name="dietaryPreferences"
+						name="dietaryPreferencesInput"
 						{value}
-						bind:group={selectedPreferences}
+						bind:group={body.dietaryPreferences}
 					/>
 				</div>
 			{/each}
@@ -170,7 +205,8 @@
 					cols="30"
 					rows="10"
 					class="bg-slate-200"
-					bind:value={specialDietaryRequests}
+					bind:value={body.specialDietaryRequests}
+					on:input={update}
 				/>
 			</div>
 		{/if}
