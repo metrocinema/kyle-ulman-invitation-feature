dcd6a4b36b7c0b2e6866ae1a4fe08069c3cde220
kyle <dev.kyleulman@gmail.com>
2022-10-22 12:34:55 -0400

    add custom fonts, verify font loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,26 @@
 	import '../app.css';
 </script>
 
-<main class="grid min-h-screen place-content-center">
+<main class="grid min-h-screen place-content-center gap-4 p-4">
+	<ul class="text-light/text">
+		<li
+			class="font-serif text-[44px] font-bold uppercase leading-none tracking-[0.25px]"
+		>
+			Kepler Bold Condensed
+		</li>
+		<li class="font-serif font-light leading-[24px]">
+			Kepler Light Caption
+		</li>
+		<li
+			class="font-serif text-[28px] font-medium leading-[32px] tracking-[-0.45px]"
+		>
+			Kepler Medium Subheading
+		</li>
+		<li class="font-sans font-normal italic leading-[24px]">
+			National 2 Regular Italic
+		</li>
+		<li class="font-sans font-normal leading-[24px]">National 2 Regular</li>
+	</ul>
+
 	<slot />
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		fontFamily: {
+			sans: ['"National 2"', ...defaultTheme.fontFamily.sans],
+			serif: ['"Kepler Std"', ...defaultTheme.fontFamily.serif]
+		},
+		extend: {
+			colors: {
+				'light/text': '#1E1F21',
+				'light/text-weak': '#6D6E6F',
+				'light/primary-text': '#3F6AC4'
+			}
+		}
 	},
 	plugins: []
 };
