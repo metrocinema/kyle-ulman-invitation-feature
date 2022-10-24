57510851917a9f596d03f4158ade0cf8ff0b5d00
kyle <dev.kyleulman@gmail.com>
2022-10-23 21:04:35 -0400

    add error page - changed the way app handles invalid codes

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import CodeInput from '$lib/code-input/CodeInput.svelte';
+
+	// svelte-ignore unused-export-let (Including these here disables the browser warning)
+	export let data, form;
+</script>
+
+<CodeInput error={$page.error?.message} />
