adda81287158d2fc265cc5cbb9469666729a9323
kyle <dev.kyleulman@gmail.com>
2022-10-24 17:10:50 -0400

    update hero - poster responds to screen resize

--- src/lib/hero/Hero.svelte
+++ src/lib/hero/Hero.svelte
@@ -2,6 +2,7 @@
 	export let posterUrl: string, showTitle: string;
 
 	// prettier-ignore
+	// * Use these to test long titles!
 	// showTitle = `Don't Be A Menace To South Central While Drinking Your Juice In The
 	// 			Hood.`
 	// 			+
@@ -9,19 +10,18 @@
 	// 			all time.`;
 </script>
 
-<section class="bg-light/background pt-6">
-	<figure class="relative flex flex-col items-center">
+<section class="flex flex-col items-center bg-light/background">
+	<figure class="relative flex max-w-[375px] flex-col items-center">
 		<img
 			src="/background.webp"
 			alt="Hero background."
 			width="375"
 			height="765"
 		/>
-		<!-- Poster placeholder 231x348 -->
 		<img
 			src={posterUrl}
 			alt="Film poster."
-			class="absolute bottom-[63px] h-[350px]"
+			class="absolute bottom-[64px] w-[70%]"
 		/>
 		<div
 			class="national-sm absolute bottom-[63px] grid max-w-[231px] translate-y-[100%] place-items-center text-center tracking-[0.02em] text-dark/text"
